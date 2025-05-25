3a80946e2db6dc2fc201ab13017c10ba9ace6ddc
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-05-25 14:09:25 +0200

    feat: actions (#7)
    * feat: init actions
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: actions not working :3
    
    * oops :3
    
    * feat: command -> client & server command
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * feat: actions in party
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: tooltip flickering
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: button clicking
    
    Co-authored-by: Mona <59416038+meowora@users.noreply.github.com>
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * feat: link action & make actions actually work
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * refactor: one command
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * fix: party members
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    * refactor: api bump
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    Co-authored-by: Gravy Boat <gravy@thatgravyboat.tech>
    Co-authored-by: Mona <59416038+meowora@users.noreply.github.com>

--- src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/ScoreboardLine.kt
+++ src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/ScoreboardLine.kt
@@ -1,19 +1,28 @@
 package me.owdding.customscoreboard.feature.customscoreboard
 
 import earth.terrarium.olympus.client.components.Widgets
+import earth.terrarium.olympus.client.components.compound.LayoutWidget
 import earth.terrarium.olympus.client.components.string.TextWidget
 import me.owdding.customscoreboard.config.MainConfig
+import me.owdding.customscoreboard.feature.customscoreboard.elements.Element
 import me.owdding.customscoreboard.utils.TextUtils.toComponent
 import me.owdding.lib.builder.LayoutFactory
 import me.owdding.lib.displays.Alignment
 import me.owdding.lib.displays.Display
 import me.owdding.lib.displays.DisplayWidget
-import net.minecraft.client.gui.layouts.LayoutElement
+import net.minecraft.Util
+import net.minecraft.client.gui.components.AbstractWidget
+import net.minecraft.client.gui.layouts.Layout
 import net.minecraft.client.gui.layouts.LayoutSettings
 import net.minecraft.network.chat.Component
+import tech.thatgravyboat.skyblockapi.helpers.McClient
+import tech.thatgravyboat.skyblockapi.utils.extentions.translated
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+import java.time.Duration
+import java.time.temporal.ChronoUnit
 
 data class ScoreboardLine(
-    val layout: LayoutElement,
+    private val layout: AbstractWidget,
     val alignment: Alignment = DEFAULT_ALIGNMENT,
     val isBlank: Boolean = false,
 ) {
@@ -26,6 +35,38 @@ data class ScoreboardLine(
         alignment,
         isBlank,
     )
+
+    private var actions: Map<Element.Actions, Any> = emptyMap()
+
+    val widget by lazy {
+        Widgets.button {
+            it.setSize(layout.width, layout.height)
+            it.withTexture(null)
+            it.withRenderer { graphics, ctx, ticks ->
+                graphics.translated(it.x, it.y) {
+                    layout.render(graphics, ctx.mouseX, ctx.mouseY, ticks)
+                }
+            }
+
+            if (Element.Actions.HOVER in actions) {
+                it.withTooltip(Text.multiline(actions[Element.Actions.HOVER]))
+                it.setTooltipDelay(Duration.of(-1, ChronoUnit.SECONDS))
+            }
+
+            it.withCallback {
+                actions.forEach { (k, v) ->
+                    when (k) {
+                        Element.Actions.COMMAND -> McClient.sendCommand((v as String).removePrefix("/"))
+                        Element.Actions.CLICK -> (v as (() -> Unit))()
+                        Element.Actions.LINK -> Util.getPlatform().openUri(v as String)
+                        else -> {}
+                    }
+                }
+            }
+
+        }
+    }
+
 
     fun applySettings(settings: LayoutSettings) {
         settings.alignHorizontally(
@@ -58,17 +99,38 @@ data class ScoreboardLine(
             is String -> this.toComponent().align()
             is Component -> this.align()
             is ScoreboardLine -> this
-            is LayoutElement -> ScoreboardLine(this)
+            is Layout -> ScoreboardLine(LayoutWidget(this))
+            is AbstractWidget -> ScoreboardLine(this)
             is Display -> ScoreboardLine(DisplayWidget(this))
+            is Pair<*, *> -> {
+                (this.second as? ActionBuilder)?.let { action ->
+                    this.first!!.toScoreboardElement()?.apply { this.actions = action.toMap() }
+                }
+            }
+
             else -> null
         }
 
         fun List<ScoreboardLine>.createColumn() = LayoutFactory.vertical {
             this@createColumn.forEach { line ->
-                widget(line.layout, line::applySettings)
+                widget(line.widget, line::applySettings)
             }
         }
     }
+}
+
+class ActionBuilder() {
+    var hover: List<String>? = null
+    var command: String? = null
+    var click: (() -> Unit)? = null
+    var link: String? = null
+
+    fun toMap() = mapOf(
+        Element.Actions.HOVER to hover,
+        Element.Actions.COMMAND to command,
+        Element.Actions.CLICK to click,
+        Element.Actions.LINK to link,
+    ).mapNotNull { (key, value) -> value?.let { key to it } }.toMap()
 }
 
 private fun String.asTextWidget() = toComponent().asTextWidget()
