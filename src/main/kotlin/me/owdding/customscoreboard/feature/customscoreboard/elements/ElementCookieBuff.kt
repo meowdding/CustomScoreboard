bafeae023d8f6f01e9ceba241c42f2a134646c6e
Gravy Boat <gravy@thatgravyboat.tech>
2025-06-27 19:09:49 -0230

    fix: cookie buff not showing years and not using number formatter

--- src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementCookieBuff.kt
+++ src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementCookieBuff.kt
@@ -2,6 +2,7 @@ package me.owdding.customscoreboard.feature.customscoreboard.elements
 
 import me.owdding.customscoreboard.AutoElement
 import me.owdding.customscoreboard.ElementGroup
+import me.owdding.customscoreboard.feature.customscoreboard.CustomScoreboardRenderer
 import tech.thatgravyboat.skyblockapi.api.profile.effects.EffectsAPI
 import tech.thatgravyboat.skyblockapi.utils.time.until
 
@@ -11,18 +12,25 @@ object ElementCookieBuff : Element() {
     override fun getDisplay(): Any {
         return if (EffectsAPI.isBoosterCookieActive) {
             val duration = EffectsAPI.boosterCookieExpireTime.until()
-            duration.toComponents { days, hours, minutes, seconds, _ ->
-                val text = buildString {
+
+            val years = duration.inWholeDays / 365
+            val days = duration.inWholeDays % 365
+            val hours = duration.inWholeHours % 24
+            val minutes = duration.inWholeMinutes % 60
+            val seconds = duration.inWholeSeconds % 60
+
+            CustomScoreboardRenderer.formatNumberDisplayDisplay(
+                "Cookie Buff",
+                buildString {
+                    if (years > 0) append("${years}y ")
                     if (days > 0) append("${days}d ")
                     if (hours > 0) append("${hours}h ")
                     if (minutes > 0) append("${minutes}m ")
-                    if (days <= 0 && seconds > 0) append("${seconds}s") // Only show seconds if there is no days
+                    if (years <= 0 && days <= 0 && seconds > 0) append("${seconds}s") // Only show seconds if there is no days or years
                     if (isEmpty()) append("0s")
-                }.trim()
-                val color = if (duration.inWholeSeconds < 60) "§6" else "§f"
-
-                "§dCookie Buff: $color$text"
-            }
+                },
+                "§d"
+            )
         } else {
             "§dCookie Buff: §cExpired"
         }.withActions {
