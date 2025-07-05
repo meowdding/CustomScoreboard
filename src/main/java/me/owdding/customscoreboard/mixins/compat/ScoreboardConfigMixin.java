c0a7cfd4c8f3efa91eb83aff3aaa8619ab9434bb
Mona <59416038+meowora@users.noreply.github.com>
2025-07-05 18:52:35 +0200

    meow :3

--- src/main/java/me/owdding/customscoreboard/mixins/compat/ScoreboardConfigMixin.java
+++ src/main/java/me/owdding/customscoreboard/mixins/compat/ScoreboardConfigMixin.java
@@ -2,7 +2,6 @@ package me.owdding.customscoreboard.mixins.compat;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.moulberry.mixinconstraints.annotations.IfModLoaded;
-import me.jfenn.scoreboardoverhaul.common.config.ConfigManager;
 import me.jfenn.scoreboardoverhaul.common.config.ScoreboardConfig;
 import me.owdding.customscoreboard.feature.ModCompat;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,15 +10,12 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Pseudo
 @IfModLoaded("scoreboard-overhaul")
-@Mixin(value = {ConfigManager.class}, remap = false)
-public class ScoreboardOverhaulConfigManagerMixin {
+@Mixin(value = ScoreboardConfig.class, remap = false)
+public class ScoreboardConfigMixin {
 
-    @ModifyReturnValue(
-        method = "access$getInstance$cp",
-        at = @At("RETURN")
-    )
-    private static ScoreboardConfig customscoreboard$access$getInstance(ScoreboardConfig original) {
-        ModCompat.INSTANCE.setScoreboardOverhaulEnabled(original.isEnabled());
+    @ModifyReturnValue(method = "isEnabled", at = @At("RETURN"))
+    private static boolean customscoreboard$isEnabled(boolean original) {
+        ModCompat.INSTANCE.setScoreboardOverhaulEnabled(original);
         return original;
     }
 
