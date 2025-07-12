8534f2aea3b7bb2c7f9195bb5c7e44c5fc23151f
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-12 17:04:00 +0200

    feat: god potion duration
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementGodPotion.kt
+++ src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementGodPotion.kt
@@ -4,13 +4,12 @@ import me.owdding.customscoreboard.AutoElement
 import me.owdding.customscoreboard.ElementGroup
 import me.owdding.customscoreboard.feature.customscoreboard.CustomScoreboardRenderer
 import tech.thatgravyboat.skyblockapi.api.profile.effects.EffectsAPI
-import tech.thatgravyboat.skyblockapi.utils.time.until
+import kotlin.time.Duration.Companion.seconds
 
 @AutoElement(ElementGroup.MIDDLE)
-object ElementCookieBuff : Element() {
-
-    override fun getDisplay(): Any {
-        val duration = EffectsAPI.boosterCookieExpireTime.until()
+object ElementGodPotion : Element() {
+    override fun getDisplay(): Any? {
+        val duration = EffectsAPI.godPotionDuration
 
         val years = duration.inWholeDays / 365
         val days = duration.inWholeDays % 365
@@ -25,13 +24,10 @@ object ElementCookieBuff : Element() {
             if (minutes > 0) append("${minutes}m ")
             if (years <= 0 && days <= 0 && seconds > 0) append("${seconds}s") // Only show seconds if there is no days or years
             if (isEmpty()) append("0s")
-        }.takeIf { EffectsAPI.isBoosterCookieActive } ?: "§cExpired"
+        }.takeIf { EffectsAPI.godPotionDuration > 0.seconds } ?: "§cExpired"
 
-        return CustomScoreboardRenderer.formatNumberDisplayDisplay("Cookie Buff", line, "§d").withActions {
-            hover = listOf("§7Click to open Booster Cookie Menu")
-            command = "/boostercookiemenu"
-        }
+        return CustomScoreboardRenderer.formatNumberDisplayDisplay("God Potion", line, "§c")
     }
 
-    override val configLine = "Cookie Buff"
+    override val configLine: String = "God Potion"
 }
