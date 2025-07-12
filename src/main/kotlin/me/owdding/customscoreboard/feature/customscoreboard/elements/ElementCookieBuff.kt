32cc17f5a43e03a13077513230bc0cf17b185ef5
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-12 17:07:17 +0200

    refactor: dedupe code
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementCookieBuff.kt
+++ src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementCookieBuff.kt
@@ -3,6 +3,7 @@ package me.owdding.customscoreboard.feature.customscoreboard.elements
 import me.owdding.customscoreboard.AutoElement
 import me.owdding.customscoreboard.ElementGroup
 import me.owdding.customscoreboard.feature.customscoreboard.CustomScoreboardRenderer
+import me.owdding.customscoreboard.utils.Utils.toFormatYears
 import tech.thatgravyboat.skyblockapi.api.profile.effects.EffectsAPI
 import tech.thatgravyboat.skyblockapi.utils.time.until
 
@@ -10,22 +11,7 @@ import tech.thatgravyboat.skyblockapi.utils.time.until
 object ElementCookieBuff : Element() {
 
     override fun getDisplay(): Any {
-        val duration = EffectsAPI.boosterCookieExpireTime.until()
-
-        val years = duration.inWholeDays / 365
-        val days = duration.inWholeDays % 365
-        val hours = duration.inWholeHours % 24
-        val minutes = duration.inWholeMinutes % 60
-        val seconds = duration.inWholeSeconds % 60
-
-        val line = buildString {
-            if (years > 0) append("${years}y ")
-            if (days > 0) append("${days}d ")
-            if (hours > 0) append("${hours}h ")
-            if (minutes > 0) append("${minutes}m ")
-            if (years <= 0 && days <= 0 && seconds > 0) append("${seconds}s") // Only show seconds if there is no days or years
-            if (isEmpty()) append("0s")
-        }.takeIf { EffectsAPI.isBoosterCookieActive } ?: "§cExpired"
+        val line = EffectsAPI.boosterCookieExpireTime.until().toFormatYears().takeIf { EffectsAPI.isBoosterCookieActive } ?: "§cExpired"
 
         return CustomScoreboardRenderer.formatNumberDisplayDisplay("Cookie Buff", line, "§d").withActions {
             hover = listOf("§7Click to open Booster Cookie Menu")
--- src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementGodPotion.kt
+++ src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementGodPotion.kt
@@ -3,6 +3,7 @@ package me.owdding.customscoreboard.feature.customscoreboard.elements
 import me.owdding.customscoreboard.AutoElement
 import me.owdding.customscoreboard.ElementGroup
 import me.owdding.customscoreboard.feature.customscoreboard.CustomScoreboardRenderer
+import me.owdding.customscoreboard.utils.Utils.toFormatYears
 import tech.thatgravyboat.skyblockapi.api.profile.effects.EffectsAPI
 import kotlin.time.Duration.Companion.seconds
 
@@ -10,21 +11,7 @@ import kotlin.time.Duration.Companion.seconds
 object ElementGodPotion : Element() {
     override fun getDisplay(): Any? {
         val duration = EffectsAPI.godPotionDuration
-
-        val years = duration.inWholeDays / 365
-        val days = duration.inWholeDays % 365
-        val hours = duration.inWholeHours % 24
-        val minutes = duration.inWholeMinutes % 60
-        val seconds = duration.inWholeSeconds % 60
-
-        val line = buildString {
-            if (years > 0) append("${years}y ")
-            if (days > 0) append("${days}d ")
-            if (hours > 0) append("${hours}h ")
-            if (minutes > 0) append("${minutes}m ")
-            if (years <= 0 && days <= 0 && seconds > 0) append("${seconds}s") // Only show seconds if there is no days or years
-            if (isEmpty()) append("0s")
-        }.takeIf { EffectsAPI.godPotionDuration > 0.seconds } ?: "§cExpired"
+        val line = duration.toFormatYears().takeIf { duration > 0.seconds } ?: "§cExpired"
 
         return CustomScoreboardRenderer.formatNumberDisplayDisplay("God Potion", line, "§c")
     }
