86a64dcf45e1251b96d4d74dfeb200727b25358a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-07-15 00:38:02 +0200

    init or some bs
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.kotlin)
+    kotlin("jvm")
 }
 
 repositories {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,104 +1,183 @@
+@file:Suppress("UnstableApiUsage")
+
+import net.msrandom.stubs.GenerateStubApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    alias(libs.plugins.kotlin)
-    alias(libs.plugins.loom)
-    alias(libs.plugins.ksp)
-}
-
-ksp {
-    arg("meowdding.modules.project_name", "CustomScoreboard")
-    arg("meowdding.modules.package", "me.owdding.customscoreboard.generated")
+    java
+    kotlin("jvm") version "2.0.0"
+    alias(libs.plugins.terrarium.cloche)
+    id("maven-publish")
+    alias(libs.plugins.kotlin.symbol.processor)
 }
 
 repositories {
-    maven("https://maven.teamresourceful.com/repository/maven-public/")
-    maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
-    maven("https://repo.hypixel.net/repository/Hypixel/")
-    maven("https://api.modrinth.com/maven")
+    maven(url = "https://maven.teamresourceful.com/repository/maven-public/")
+    maven(url = "https://repo.hypixel.net/repository/Hypixel/")
+    maven(url = "https://api.modrinth.com/maven")
+    maven(url = "https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
+    maven(url = "https://maven.nucleoid.xyz")
+    maven(url = "https://maven.msrandom.net/repository/cloche")
+    maven(url = "https://maven.msrandom.net/repository/root")
+    mavenCentral()
+    mavenLocal()
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        languageVersion = KotlinVersion.KOTLIN_2_0
+        freeCompilerArgs.addAll(
+            "-Xmulti-platform",
+            "-Xno-check-actual",
+            "-Xexpect-actual-classes",
+        )
+    }
 }
 
 dependencies {
-    compileOnly(libs.meowdding.ktmodules)
     ksp(libs.meowdding.ktmodules)
-    compileOnly(project(":annotations"))
+
+    compileOnly(libs.kotlin.stdlib)
+
     ksp(project(":annotations"))
-
-    minecraft(libs.minecraft)
-    mappings(loom.layered {
-        officialMojangMappings()
-        parchment("org.parchmentmc.data:parchment-1.21.5:2025.04.19@zip")
-    })
-    modImplementation(libs.loader)
-    modImplementation(libs.fabrickotlin)
-    modImplementation(libs.fabric)
-
-    modImplementation(libs.hypixelapi)
-    modImplementation(libs.skyblockapi)
-    modImplementation(libs.rconfig)
-    modImplementation(libs.rconfigkt)
-    modImplementation(libs.rlib)
-    modImplementation(libs.olympus)
-    modImplementation(libs.meowdding.lib)
-    modImplementation(libs.mixinconstraints)
-
-    include(libs.hypixelapi)
-    include(libs.skyblockapi)
-    include(libs.rconfig)
-    include(libs.rconfigkt)
-    include(libs.rlib)
-    include(libs.olympus)
-    include(libs.meowdding.lib)
-    include(libs.mixinconstraints)
-
-    modCompileOnly("maven.modrinth:scoreboard-overhaul:1.4.0-mc1.21.5")
-
-    modRuntimeOnly(libs.devauth)
 }
 
-loom {
-    runs {
-        getByName("client") {
-            property("devauth.configDir", rootProject.file(".devauth").absolutePath)
+cloche {
+    metadata {
+        modId = "customscoreboard"
+        name = "Custom Scoreboard"
+        license = ""
+        clientOnly = true
+    }
+
+    common {
+        mixins.from("src/mixins/customscoreboard.mixins.json")
+
+        dependencies {
+            compileOnly(project(":annotations"))
+            compileOnly(libs.meowdding.ktmodules)
+
+            modImplementation(libs.hypixelapi)
+            modImplementation(libs.skyblockapi)
+
+            modImplementation(libs.mixinconstraints)
+
+            modImplementation(libs.fabric.language.kotlin)
+
+            modImplementation(libs.skyblockapi)
+            modImplementation(libs.meowdding.lib)
+
+            modRuntimeOnly(libs.devauth)
+            modRuntimeOnly(libs.modmenu)
         }
+    }
+
+    fun createVersion(
+        name: String,
+        version: String = name,
+        loaderVersion: Provider<String> = libs.versions.fabric.loader,
+        fabricApiVersion: Provider<String> = libs.versions.fabric.api,
+        dependencies: MutableMap<String, Provider<MinimalExternalModuleDependency>>.() -> Unit = { },
+    ) {
+        val dependencies = mutableMapOf<String, Provider<MinimalExternalModuleDependency>>().apply(dependencies)
+        val rlib = dependencies["resourcefullib"]!!
+        val rconfig = dependencies["resourcefulconfig"]!!
+        val olympus = dependencies["olympus"]!!
+
+        fabric(name) {
+            includedClient()
+            minecraftVersion = version
+            this.loaderVersion = loaderVersion.get()
+
+            include(libs.hypixelapi)
+            include(libs.skyblockapi)
+            include(libs.meowdding.lib)
+            include(rlib)
+            include(olympus)
+            include(rconfig)
+            include(libs.resourcefulkt.config)
+
+            metadata {
+                entrypoint("client") {
+                    adapter = "kotlin"
+                    value = "me.owdding.customscoreboard.Main"
+                }
+
+                fun dependency(modId: String, version: Provider<String>) {
+                    dependency {
+                        this.modId = modId
+                        this.required = true
+                        version {
+                            this.start = version
+                        }
+                    }
+                }
+
+                dependency("fabric-language-kotlin", libs.versions.fabric.language.kotlin)
+                dependency("resourcefullib", rlib.map { it.version!! })
+                dependency("skyblock-api", libs.versions.skyblockapi)
+                dependency("olympus", olympus.map { it.version!! })
+                dependency("meowdding-lib", libs.versions.meowdding.lib)
+            }
+
+            dependencies {
+                fabricApi(fabricApiVersion, minecraftVersion)
+                modImplementation(olympus)
+                modImplementation(rconfig)
+                modImplementation(libs.resourcefulkt.config)
+            }
+
+            runs {
+                client()
+            }
+        }
+    }
+
+    createVersion("1.21.5", fabricApiVersion = provider { "0.127.1" }) {
+        this["resourcefullib"] = libs.resourceful.lib1215
+        this["resourcefulconfig"] = libs.resourceful.config1215
+        this["olympus"] = libs.olympus.lib1215
+    }
+    createVersion("1.21.7") {
+        this["resourcefullib"] = libs.resourceful.lib1217
+        this["resourcefulconfig"] = libs.resourceful.config1217
+        this["olympus"] = libs.olympus.lib1217
+    }
+
+    mappings { official() }
+}
+
+tasks.named("createCommonApiStub", GenerateStubApi::class) {
+    excludes.add(libs.skyblockapi.get().module.toString())
+    excludes.add(libs.meowdding.lib.get().module.toString())
+    project(":annotations").let {
+        excludes.add("${it.group}:${it.name}:${it.version}")
     }
 }
 
 tasks {
-    processResources {
-        inputs.property("version", project.version)
-        filesMatching("fabric.mod.json") {
-            expand(
-                "version" to project.version,
-                "minecraft" to libs.versions.minecraft.get(),
-                "fabricLoader" to libs.versions.loader.get(),
-                "fabricLanguageKotlin" to libs.versions.fabrickotlin.get(),
-                "meowddingLib" to libs.versions.meowdding.lib.get(),
-                "resourcefullib" to libs.versions.rlib.get(),
-                "skyblockApi" to libs.versions.sbapi.get(),
-                "olympus" to libs.versions.olympus.get(),
-                "resourcefulconfigkt" to libs.versions.rconfigkt.get(),
-                "resourcefulconfig" to libs.versions.rconfig.get(),
-            )
-        }
-    }
-
-    jar {
-        from("LICENSE")
-    }
-
     compileKotlin {
         compilerOptions {
             jvmTarget = JvmTarget.JVM_21
         }
     }
 
+    withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(21)
+    }
 }
 
 java {
     withSourcesJar()
 }
 
-kotlin {
-    jvmToolchain(21)
+
+ksp {
+    arg("meowdding.modules.project_name", "CustomScoreboard")
+    arg("meowdding.modules.package", "me.owdding.customscoreboard.generated")
+    this@ksp.excludedSources.from(sourceSets.getByName("1215").kotlin.srcDirs)
+    this@ksp.excludedSources.from(sourceSets.getByName("1217").kotlin.srcDirs)
 }
