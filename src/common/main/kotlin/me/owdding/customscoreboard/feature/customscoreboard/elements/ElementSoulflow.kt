a97d39f5c4de9c9c04f9ebcb58681178ce568d0e
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2025-10-08 17:45:28 +0200

    refactor: hide soulflow when not enabled in tab
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/common/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementSoulflow.kt
+++ src/common/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementSoulflow.kt
@@ -4,9 +4,16 @@ import me.owdding.customscoreboard.feature.customscoreboard.CustomScoreboardRend
 import me.owdding.customscoreboard.feature.customscoreboard.NumberTrackingElement
 import me.owdding.customscoreboard.utils.NumberUtils.format
 import me.owdding.customscoreboard.utils.ScoreboardElement
+import me.owdding.customscoreboard.utils.TextUtils.anyMatch
+import me.owdding.ktmodules.Module
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyWidget
+import tech.thatgravyboat.skyblockapi.api.events.info.TabWidget
+import tech.thatgravyboat.skyblockapi.api.events.info.TabWidgetChangeEvent
 import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.api.profile.CurrencyAPI
 
+@Module
 @ScoreboardElement
 object ElementSoulflow : Element(), NumberTrackingElement {
     override var previousAmount: Long = -1
@@ -20,9 +27,20 @@ object ElementSoulflow : Element(), NumberTrackingElement {
         return CustomScoreboardRenderer.formatNumberDisplayDisplay("Soulflow", line, numberColor)
     }
 
+    override fun showWhen() = soulflowInTablist
+
     override fun showIsland() = !SkyBlockIsland.inAnyIsland(SkyBlockIsland.THE_RIFT)
 
     override val configLine = "Soulflow"
     override val id = "SOULFLOW"
-    override val configLineHover = listOf("Requires the Soulflow option enabled in the Profile category in /tablist.")
+    override val configLineHover = listOf("Requires the Soulflow option enabled in the Profile category in /tablist.", "Will not show if disabled.")
+
+    private var soulflowInTablist = false
+    private val soulflowRegex = " Soulflow: .*".toRegex()
+
+    @Subscription
+    @OnlyWidget(TabWidget.PROFILE)
+    fun onTab(event: TabWidgetChangeEvent) {
+        soulflowInTablist = soulflowRegex.anyMatch(event.new)
+    }
 }
