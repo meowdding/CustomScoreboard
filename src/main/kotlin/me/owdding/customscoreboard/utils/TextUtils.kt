f342abda4c824fcf6319d47e25d70333cc1549d3
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-01-31 13:04:18 +0100

    fix: duping text in the same lines Co-authored-by: Gravy Boat <gravy@thatgravyboat.tech>
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/customscoreboard/utils/TextUtils.kt
+++ src/main/kotlin/me/owdding/customscoreboard/utils/TextUtils.kt
@@ -1,37 +1,118 @@
 package me.owdding.customscoreboard.utils
 
+import net.minecraft.ChatFormatting
 import net.minecraft.network.chat.Component
-import tech.thatgravyboat.skyblockapi.utils.text.Text
-
-private val colorCodesStart = Regex("^(?<start>(§.| )*)(?!§.| )")
-private val colorCodesEnd = Regex("^(?<end>(.§| )*)(?!.§| )")
+import net.minecraft.network.chat.Style
+import java.util.Optional
 
 object TextUtils {
 
     fun String.toComponent() = Component.literal(this)
 
-    fun String.trimStartIgnoreColor(): String {
-        val start = colorCodesStart.find(this)?.groups?.get("start")?.value ?: ""
-        val trimmed = this.removePrefix(start)
-        return start.replace(" ", "") + trimmed
+    val Component.spans: List<Pair<String, Style>>
+        get() {
+            val output = mutableListOf<Pair<String, Style>>()
+            this.visit(
+                { style, content ->
+                    output.add(content to style)
+                    Optional.empty<Unit>()
+                },
+                Style.EMPTY,
+            )
+            return output
+        }
+
+    fun String.trimIgnoreColor(start: Boolean = true, end: Boolean = true): String {
+        return this.tryTrimIgnoreColor(start, end).second
     }
 
-    fun String.trimEndIgnoreColor(): String {
-        val end = colorCodesEnd.find(this.reversed())?.groups?.get("end")?.value?.reversed() ?: ""
-        val trimmed = this.removeSuffix(end)
-        return trimmed + end.replace(" ", "")
+    fun String.tryTrimIgnoreColor(start: Boolean = true, end: Boolean = true): Pair<Boolean, String> {
+        var middle = this
+        val prefix = StringBuilder()
+        val suffix = StringBuilder()
+
+        if (start) {
+            var i = 0
+            var include = false
+
+            while (i < middle.length) {
+                val char = middle[i]
+                when {
+                    include -> prefix.append(char)
+                    char == ChatFormatting.PREFIX_CODE -> {
+                        include = true
+                        prefix.append(char)
+                    }
+
+                    !char.isWhitespace() -> break
+                }
+                i++
+            }
+
+            middle = middle.substring(i)
+        }
+
+        if (end) {
+            var i = middle.length - 1
+            var include = false
+
+            while (i >= 0) {
+                val char = middle[i]
+
+                when {
+                    include -> suffix.append(char)
+                    i - 1 >= 0 && middle[i - 1] == ChatFormatting.PREFIX_CODE -> {
+                        include = true
+                        suffix.append(char)
+                    }
+
+                    !char.isWhitespace() -> break
+                }
+                i--
+            }
+
+            middle = middle.take(i + 1)
+        }
+
+        if (middle.isEmpty()) {
+            return true to prefix.toString() + suffix.reverse().toString()
+        }
+
+        return false to prefix.toString() + middle + suffix.reverse().toString()
     }
 
     fun Component.trim(): Component {
-        var siblings = this.toFlatList().toMutableList()
-        val first = siblings.firstOrNull()
-        val last = siblings.lastOrNull()
-        siblings = siblings.drop(1).dropLast(1).toMutableList()
+        val spans = this.spans.toMutableList()
+        return when {
+            spans.isEmpty() -> Component.empty()
+            spans.size == 1 -> Component.literal(spans[0].first.trimIgnoreColor()).setStyle(spans[0].second)
+            else -> {
+                val prefix = mutableListOf<Component>()
+                val suffix = mutableListOf<Component>()
 
-        siblings.addFirst(first?.let { Component.literal(it.string.trimStartIgnoreColor()).withStyle(it.style) })
-        siblings.addLast(last?.let { Component.literal(it.string.trimEndIgnoreColor()).withStyle(it.style) })
+                while (spans.isNotEmpty()) {
+                    val (content, style) = spans.removeFirst()
+                    val (isEmpty, trimmed) = content.tryTrimIgnoreColor(end = false)
+                    prefix.add(Component.literal(trimmed).setStyle(style))
 
-        return Text.join(*siblings.toTypedArray())
+                    if (!isEmpty) break // Means we found actual content
+                }
+
+                while (spans.isNotEmpty()) {
+                    val (content, style) = spans.removeLast()
+                    val (isEmpty, trimmed) = content.tryTrimIgnoreColor(start = false)
+                    suffix.addFirst(Component.literal(trimmed).setStyle(style))
+
+                    if (!isEmpty) break // Means we found actual content
+                }
+
+                Component.empty().apply {
+                    prefix.forEach(this::append)
+                    spans.forEach { (content, style) -> this.append(Component.literal(content).setStyle(style)) }
+                    suffix.forEach(this::append)
+                }
+            }
+        }
     }
 
     fun Component.isBlank() = this.string.isBlank()
