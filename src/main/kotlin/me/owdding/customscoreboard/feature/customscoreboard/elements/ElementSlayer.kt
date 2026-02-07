47223083cde8febbdb021668c3a256b8473b7add
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-02-07 20:58:37 +0100

    fix: slayer widget tablist fallback
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementSlayer.kt
+++ src/main/kotlin/me/owdding/customscoreboard/feature/customscoreboard/elements/ElementSlayer.kt
@@ -1,11 +1,18 @@
 package me.owdding.customscoreboard.feature.customscoreboard.elements
 
 import me.owdding.customscoreboard.utils.ScoreboardElement
+import me.owdding.customscoreboard.utils.TextUtils.trim
 import me.owdding.customscoreboard.utils.Utils.sublistFromFirst
+import me.owdding.ktmodules.Module
 import net.minecraft.network.chat.Component
+import tech.thatgravyboat.skyblockapi.api.events.base.Subscription
+import tech.thatgravyboat.skyblockapi.api.events.base.predicates.OnlyWidget
 import tech.thatgravyboat.skyblockapi.api.events.info.ScoreboardUpdateEvent
+import tech.thatgravyboat.skyblockapi.api.events.info.TabWidget
+import tech.thatgravyboat.skyblockapi.api.events.info.TabWidgetChangeEvent
 import tech.thatgravyboat.skyblockapi.utils.regex.component.ComponentRegex
 
+@Module
 @ScoreboardElement
 object ElementSlayer : Element() {
     override fun getDisplay() = formattedLines
@@ -20,5 +27,12 @@ object ElementSlayer : Element() {
 
     override fun onScoreboardUpdate(event: ScoreboardUpdateEvent) {
         formattedLines = event.components.sublistFromFirst(3, slayerQuestRegex::matches)
+    }
+
+    @Subscription
+    @OnlyWidget(TabWidget.SLAYER)
+    fun onTabWidget(event: TabWidgetChangeEvent) {
+        if (formattedLines.isNotEmpty()) return
+        formattedLines = event.newComponents.sublistFromFirst(3, slayerQuestRegex::matches).map { it.trim() }
     }
 }
