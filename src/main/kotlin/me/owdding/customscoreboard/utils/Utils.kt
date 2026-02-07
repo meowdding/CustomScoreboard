3d53e77111b3cac03f5970666f3b290b51dfc7a7
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2026-02-07 15:42:04 +0100

    fix: Date Element using the wrong winter color
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/owdding/customscoreboard/utils/Utils.kt
+++ src/main/kotlin/me/owdding/customscoreboard/utils/Utils.kt
@@ -6,7 +6,6 @@ import com.teamresourceful.resourcefulconfigkt.api.RConfigKtEntry
 import com.teamresourceful.resourcefulconfigkt.api.builders.CategoryBuilder
 import me.owdding.ktmodules.AutoCollect
 import net.minecraft.network.chat.Component
-import tech.thatgravyboat.skyblockapi.api.datetime.SkyBlockSeason
 import tech.thatgravyboat.skyblockapi.api.profile.effects.EffectsAPI
 import tech.thatgravyboat.skyblockapi.utils.regex.component.ComponentRegex
 import tech.thatgravyboat.skyblockapi.utils.text.Text
@@ -19,21 +18,6 @@ import kotlin.time.Duration.Companion.seconds
 
 object Utils {
     fun hasCookieActive() = EffectsAPI.boosterCookieExpireTime.until() > 1.seconds
-
-    private val seasonColors = mapOf(
-        SkyBlockSeason.EARLY_SPRING to "§a",
-        SkyBlockSeason.SPRING to "§a",
-        SkyBlockSeason.LATE_SPRING to "§a",
-        SkyBlockSeason.EARLY_SUMMER to "§e",
-        SkyBlockSeason.SUMMER to "§e",
-        SkyBlockSeason.LATE_SUMMER to "§e",
-        SkyBlockSeason.EARLY_AUTUMN to "§6",
-        SkyBlockSeason.AUTUMN to "§6",
-        SkyBlockSeason.LATE_AUTUMN to "§6",
-        SkyBlockSeason.EARLY_WINTER to "§9",
-        SkyBlockSeason.WINTER to "§9",
-        SkyBlockSeason.LATE_WINTER to "§9",
-    )
 
     fun Duration.toFormatYears() = buildString {
         val years = inWholeDays / 365
@@ -49,8 +33,6 @@ object Utils {
         if (years <= 0 && days <= 0 && seconds > 0) append("${seconds}s") // Only show seconds if there is no days or years
         if (isEmpty()) append("0s")
     }.trim()
-
-    fun SkyBlockSeason.getColoredName(): String = seasonColors[this] + this.toString()
 
     fun <T> Collection<T>.nextAfter(element: T, skip: Int = 1): T? {
         val index = indexOfFirst { if (it is Component && element is String) it.stripped == element else it == element }
